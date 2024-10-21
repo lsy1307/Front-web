1353302541da8ef2a5f003e48eb7729e90406ff1
lsy1307 <lsy1307@inha.edu>
2024-10-21 15:12:28 +0900

    Feat: Qna Home 구현

--- src/pages/Qna/QnaHome.jsx
+++ src/pages/Qna/QnaHome.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import MainComponent from '../../components/pages/Home/Main/MainComponent/MainComponent';
+import QnaHomeComponent from '../../components/pages/Qna/QnaHome/QnaHomeComponent';
 
 const QnaHome = () => {
-  return <></>;
+  return <MainComponent contents={<QnaHomeComponent />} />;
 };
 
 export default QnaHome;

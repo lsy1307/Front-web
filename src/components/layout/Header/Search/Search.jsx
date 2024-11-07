e19f91f0f4798c5da29bfa87b510437d38e5b019
lsy1307 <lsy1307@inha.edu>
2024-11-07 06:57:45 +0000

    Fix: 검색 버튼 오류 수정

--- src/components/layout/Header/Search/Search.jsx
+++ src/components/layout/Header/Search/Search.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SearchContainer, SearchBox, SearchIcon } from './style';
+import searchIcon from '../../../../assets/svgs/search_button.svg';
 
 const Search = () => {
   return (
     <SearchContainer>
-      <SearchIcon src="../src/assets/svgs/search_button.svg" />
+      <SearchIcon src={searchIcon} />
       <SearchBox placeholder="Search..." />
     </SearchContainer>
   );

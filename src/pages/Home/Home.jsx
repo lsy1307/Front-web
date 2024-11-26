3a205c54bbc320e369ad1df08ae913fb3e52f612
lsy1307 <lsy1307@inha.edu>
2024-11-26 06:19:58 +0000

    Fix: 오류 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainComponent from '../../components/pages/Home/Main/MainComponent/MainComponent';
 import RequestMain from '../../components/pages/Main/RequestMain';
-import StRadar from '../../components/common/Radar/STRadar';
 const Home = () => {
   return (
     <MainComponent
